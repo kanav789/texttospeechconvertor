c870e6f5f1ac3e178954e109ac76325d638479c6
kanav kumar <106293653+kanav789@users.noreply.github.com>
2023-09-28 00:04:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let speech = new SpeechSynthesisUtterance();
 
 let voices = [];
 let voiceselect = document.querySelector("select");
+
+//  for changing voices
+
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
   speech.voice = voices[0];
@@ -9,10 +12,44 @@ window.speechSynthesis.onvoiceschanged = () => {
     (voice, i) => (voiceselect.options[i] = new Option(voice.name, i))
   );
 };
+
 voiceselect.addEventListener("change", () => {
   speech.voice = voices[voiceselect.value];
 });
-document.querySelector("button").addEventListener("click", () => {
+
+//  for speaking
+document.querySelector("#button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
   window.speechSynthesis.speak(speech);
+});
+
+// for speaking
+document.querySelector("#button").addEventListener("click", () => {
+  speech.text = document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);
+});
+
+// Add download button functionality
+document.querySelector("#download-button").addEventListener("click", () => {
+  // Ensure there is text to synthesize
+  if (!speech.text) {
+    alert("Please enter text to synthesize.");
+    return;
+  }
+
+  // Synthesize the speech and create an audio blob
+  let audioBlob = new Blob([speech.text], { type: "audio/mp3" });
+
+  // Create a download link
+  let downloadLink = document.createElement("a");
+  downloadLink.href = URL.createObjectURL(audioBlob);
+  downloadLink.download = "speech.mp3";
+  downloadLink.style.display = "none";
+
+  // Add the download link to the document and trigger a click event
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+
+  // Clean up by removing the download link
+  document.body.removeChild(downloadLink);
 });
